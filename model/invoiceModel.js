b6a93acbce2fbe8d9175c7bf520ce51b31d6b3d7
codecrafft <tkteamproject02@gmail.com>
2025-02-28 01:50:45 -0800

    feat: added CRUD API for invoices

---
+++ model/invoiceModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const invoiceSchema = new mongoose.Schema({
+  invoiceNumber: { type: String, required: true, unique: true },
+  client: { type: mongoose.Schema.Types.ObjectId, ref: "clients", required: true },
+  campaignName: { type: String, required: true }, // Store campaign name as a string
+  companyName: { type: String, required: true },
+  month: { type: String, required: true },
+  totalAmount: { type: Number, required: true },
+  location: { type: String, required: true }
+}, { timestamps: true }); 
+
+module.exports = mongoose.model("Invoice", invoiceSchema);
