eb96c317a625c76a7f370439dbc28916dfbdb52f
codecrafft <tkteamproject02@gmail.com>
2025-03-10 14:35:30 -0700

    update:#181 add new filed client logo and price

--- model/clientModel.js
+++ model/clientModel.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const clientSchema = new mongoose.Schema({
+  client_logo: { type: String },
   client_name: { type: String, required: true },
   client_email: { type: String, required: true, unique: true },
+  //can add match also if required
   company_name: { type: String, required: true },
   additional_companies: [{ type: String }], 
   address: { type: String, required: true },
