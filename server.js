8fa5371b32dd0bf0ea665746de18b401ec304819
codecrafft <tkteamproject02@gmail.com>
2025-03-14 14:36:07 -0700

    Cors

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const fileUpload = require("express-fileupload");
 const app = express();
 const allowedOrigins = [
     process.env.FRONTEND_URL, 
-    "http://localhost:5173"
+    "http://localhost:5173",
+	"https://streetsight.vercel.app/"
 ].filter(Boolean);
 app.use(cors({ 
     origin: allowedOrigins,
