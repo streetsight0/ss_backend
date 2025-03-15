ff85708d5a604451161e65b4a81363a03e8e9528
codecrafft <tkteamproject02@gmail.com>
2025-03-14 20:33:13 -0700

    Feat:#200 updated the logic for storing images on cloud

--- routes/campaignRoute.js
+++ routes/campaignRoute.js
@@ -2,7 +2,23 @@ const express = require("express");
 const { createCampaign, getCampaigns, getCampaignById, updateCampaign, deleteCampaign } = require("../controller/campaignController");
 const { upload } = require("../config/cloudinary");
 const route = express.Router();
-route.post("/createcampaigns", upload.array("campaign_images", 5), createCampaign); // Accepts up to 5 images
+// route.post(
+// 	"/createcampaigns",
+// 	upload.array("campaign_images", 5),
+// 	createCampaign
+// ); // Accepts up to 5 images
+
+route.post(
+	"/createcampaigns",
+	upload.array("campaign_images", 5), // Multer middleware
+	(req, res, next) => {
+		console.log("Received Headers:", req.headers);
+		console.log("Received Body:", req.body);
+		console.log("Received Files:", req.files);
+		next();
+	},
+	createCampaign
+);
 route.get("/getcampaigns", getCampaigns);
 route.get("/getcampaigns/:id", getCampaignById);
 route.put("/updatecampaigns/:id", upload.array("campaign_images", 5), updateCampaign);
