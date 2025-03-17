9d69726398fd03ab4e02a19cdf51ba8bbd43da64
codecrafft <tkteamproject02@gmail.com>
2025-03-17 02:11:34 -0700

    refactor:#200 modify end point to get clients from cmpaign

--- routes/campaignRoute.js
+++ routes/campaignRoute.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const { createCampaign, getCampaigns, getCampaignById, updateCampaign, deleteCampaign } = require("../controller/campaignController");
+const {
+	createCampaign,
+	getCampaigns,
+	getCampaignById,
+	updateCampaign,
+	deleteCampaign,
+	getCampaignsByClient,
+} = require("../controller/campaignController");
 const { upload } = require("../config/cloudinary");
 const route = express.Router();
 // route.post(
@@ -11,15 +18,11 @@ const route = express.Router();
 route.post(
 	"/createcampaigns",
 	upload.array("campaign_images", 5), // Multer middleware
-	(req, res, next) => {
-		console.log("Received Headers:", req.headers);
-		console.log("Received Body:", req.body);
-		console.log("Received Files:", req.files);
-		next();
-	},
+	
 	createCampaign
 );
 route.get("/getcampaigns", getCampaigns);
+route.get("/getcampaigns/:clientId", getCampaignsByClient);
 route.get("/getcampaigns/:id", getCampaignById);
 route.put("/updatecampaigns/:id", upload.array("campaign_images", 5), updateCampaign);
 route.delete("/deletecampaigns/:id", deleteCampaign);
